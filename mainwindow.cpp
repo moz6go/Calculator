2fe83eb9e8dac237bba25fda570f0f1d9c021e4d
mzyblikevych <moz6go@gmail.com>
2019-03-13 17:56:38 +0200

    minor fix

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
+    DefaultInit();
     ui->setupUi(this);
-	isNewOper = false;
-	firstNum = 0;
-	strOperation = "+";
+
     connect(ui->pushButton_0, SIGNAL(clicked()), this, SLOT(DigitsNumbers()));
 	connect(ui->pushButton_1, SIGNAL(clicked()), this, SLOT(DigitsNumbers()));
 	connect(ui->pushButton_2, SIGNAL(clicked()), this, SLOT(DigitsNumbers()));
@@ -26,35 +22,40 @@ MainWindow::MainWindow(QWidget *parent) :
 	connect(ui->pushButton_reverse, SIGNAL(clicked()), this, SLOT(Operations()));
 	connect(ui->pushButton_percent, SIGNAL(clicked()), this, SLOT(Operations()));
 }
-void MainWindow::DigitsNumbers()
-{
+
+void MainWindow::DefaultInit() {
+    isNewOper = false;
+    firstNum = 0;
+    secondNum = 0;
+    strOperation = "+";
+}
+
+void MainWindow::DigitsNumbers() {
 	QPushButton* button = static_cast<QPushButton*>(sender());
-	if (isNewOper)
-	{
-		ui->showResult->setText("");
-	}
+    if (isNewOper) {
+        ui->showResult->setText(ui->pushButton_0->text());
+    }
+    if (afterEqual) {
+        on_pushButton_AC_clicked();
+    }
 	double dNumber = (ui->showResult->text() + button->text()).toDouble();
 	ui->showResult->setText(QString::number(dNumber, 'g', 12));
 	isNewOper = false;
+    afterEqual = false;
 }
 double MainWindow::ResOperation(const double& first, const double& second, const QString& mathOper)
 {
-	if (mathOper == "+")
-	{
+    if (mathOper == "+") {
 		return first + second;
 	}
-	else if (mathOper == "-")
-	{
+    else if (mathOper == "-") {
 		return first - second;
 	}
-	else if (mathOper == "*")
-	{
+    else if (mathOper == "*") {
 		return first * second;
 	}
-	else if (mathOper == "/")
-	{
-		if (second)
-		{
+    else if (mathOper == "/") {
+        if (second)	{
 			return first / second;
 		}
 	}
@@ -63,63 +64,54 @@ double MainWindow::ResOperation(const double& first, const double& second, const
 void MainWindow::Operations()
 {
     QPushButton* button = static_cast<QPushButton*>(sender());
-    if(button->text() == "+/-")
-    {
+    if(button->text() == "+/-") {
 		firstNum = (ui->showResult->text()).toDouble()*(-1);
 		ui->showResult->setText(QString::number(firstNum, 'g', 12));
     }
-    else if(button->text() == "%")
-    {
-		firstNum = (ui->showResult->text()).toDouble()*0.01;
+    else if(button->text() == "%") {
+        firstNum = (ui->showResult->text()).toDouble() * 0.01;
 		ui->showResult->setText(QString::number(firstNum, 'g', 12));
     }
 }
-void MainWindow::MathOperations()
-{
-	if (!isNewOper) 
-	{
+void MainWindow::MathOperations() {
+    if (!isNewOper) {
+        afterEqual = false;
 		QPushButton* button = static_cast<QPushButton*>(sender());
 		ui->showExpression->setText(ui->showExpression->text() + " " + ui->showResult->text() + " " + button->text());
 		secondNum = (ui->showResult->text()).toDouble();
-		if(!secondNum && strOperation == "/")
-		{
+        if(!secondNum && strOperation == "/") {
 			ui->showResult->setText("Error: DIV/0");
 			ui->showExpression->setText("");
 		}
-		else
-		{
+        else {
 			ui->showResult->setText(QString::number(ResOperation(firstNum, secondNum, strOperation), 'g', 12));
 			strOperation = button->text();
+            isNewOper = true;
 			firstNum = (ui->showResult->text()).toDouble();
-			isNewOper = true;
 		}
 	}
 }
-void MainWindow::on_pushButton_equal_clicked()
-{
+
+void MainWindow::on_pushButton_equal_clicked() {
 	MathOperations();
 	ui->showExpression->setText("");
-	isNewOper = false;
+    isNewOper = false;
+    afterEqual = true;
 }
 
-void MainWindow::on_pushButton_dot_clicked()
-{
-    if(!(ui->showResult->text().contains('.')))
-    {
+void MainWindow::on_pushButton_dot_clicked() {
+    if(!(ui->showResult->text().contains('.')))     {
         ui->showResult->setText(ui->showResult->text()+'.');
     }
 }
 
-void MainWindow::on_pushButton_AC_clicked()
-{
+void MainWindow::on_pushButton_AC_clicked() {
     ui->showResult->setText(ui->pushButton_0->text());
-	ui->showExpression->setText("");
-	isNewOper = false;
-	firstNum = 0;
-	strOperation = "+";
+    ui->showExpression->setText("");
+
+    DefaultInit();
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,26 +2,24 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtDebug>
+#include <QKeyEvent>
 
 namespace Ui {
 class MainWindow;
 }
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
     Q_OBJECT
 
-public:
-    explicit MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
-
-private:
     Ui::MainWindow *ui;
 	QString strOperation;
 	bool isNewOper;
+    bool afterEqual;
 	double firstNum;
 	double secondNum;
 	double ResOperation(const double& first, const double& second, const QString& mathOper);
+    void DefaultInit();
 private slots:
     void DigitsNumbers();
     void Operations();
@@ -29,6 +27,9 @@ private slots:
     void on_pushButton_dot_clicked();
     void on_pushButton_AC_clicked();
     void on_pushButton_equal_clicked();
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
 };
 
 #endif // MAINWINDOW_H
